64f7477de3ff393f7bccb721942e9032ff785e55
Chin <chin.bimbo@gmail.com>
2014-04-14 23:38:34 -0400

    add analytics and ads to help

--- src/com/chin/bbdb/activity/HelpActivity.java
+++ src/com/chin/bbdb/activity/HelpActivity.java
@@ -1,6 +1,9 @@
 package com.chin.bbdb.activity;
 
 import com.chin.bbdb.R;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import android.support.v4.app.NavUtils;
 import android.text.Html;
@@ -19,13 +22,19 @@ public class HelpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        // set the help text
         TextView tv = (TextView) findViewById(R.id.textView_help);
         tv.setText(Html.fromHtml(getString(R.string.help_text)));
+
+        // Look up the AdView as a resource and load a request.
+        AdView adView = (AdView)this.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         // Inflate the menu; this adds items to the action bar if it is present.
         //getMenuInflater().inflate(R.menu.help, menu);
         return true;
@@ -39,5 +48,19 @@ public class HelpActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Google Analytics
+        EasyTracker.getInstance(this).activityStart(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Google Analytics
+        EasyTracker.getInstance(this).activityStop(this);
     }
 }
