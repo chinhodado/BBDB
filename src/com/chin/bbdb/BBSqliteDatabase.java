92f9552da71b16624f24a4d08fe5851f1a09ac98
Chin <chin.bimbo@gmail.com>
2015-04-21 00:53:23 -0400

    Update offline db

--- src/com/chin/bbdb/BBSqliteDatabase.java
+++ src/com/chin/bbdb/BBSqliteDatabase.java
@@ -11,7 +11,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 public class BBSqliteDatabase extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "database.db";
-    private static final int DATABASE_VERSION = 20150315;
+    private static final int DATABASE_VERSION = 20150420;
 
     public BBSqliteDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
